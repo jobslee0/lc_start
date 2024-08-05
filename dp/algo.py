48b0c6c9b32ea44a18ebd5e97f6fd495f5cf2e6f
jobslee0 <jobs.lee@outlook.com>
2024-08-06 00:14:22 +0800

    lc240806

--- dp/algo.py
+++ dp/algo.py
@@ -27,3 +27,23 @@ def lengthOfLIS(self, nums):
                 dp[i] = max(dp[i], dp[j] + 1)
         res = max(res, dp[i])
     return res
+
+def coinChange(self, coins, amount):
+    maxA = amount + 1
+    dp = [maxA for _ in range(amount + 1)]
+    dp[0] = 0
+    for i in range(1, maxA):
+        for j in range(len(coins)):
+            if coins[j] <= i:
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+    return -1 if dp[amount] > amount else dp[amount]
+
+def minDistance(self, word1, word2):
+    m,n = len(word1),len(word2)
+    dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+    for i in range(m+1):dp[i][0]=i
+    for j in range(n+1):dp[0][j]=j
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            dp[i][j] = min(dp[i-1][j-1] + (0 if word1[i-1]==word2[j-1] else 1),dp[i-1][j]+1,dp[i][j-1]+1)
+    return dp[m][n]
